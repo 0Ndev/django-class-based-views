373694bd5c658d2a65c5e451050fddaed289ae3c
Natalia <natalialllepost@gmail.com>
2021-04-29 11:31:44 +0200

    list view and detail view

--- advanced_cbv/urls.py
+++ advanced_cbv/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import url, include
 from basic_app import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     url(r'^$', views.IndexView.as_view()),
+    url(r'^basic_app/', include('basic_app.urls', namespace='basic_app')),
     # url(r'^$', views.CBView.as_view()),
     # url(r'^$', views.index, name='index'),
-    # url(r'^basic_app/', include('basic_app.urls')),
-    path('admin/', admin.site.urls),
 ]
--- basic_app/views.py
+++ basic_app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from . import models
 # from django.http import HttpResponse
 
 
 # def index(req):
 #     return render(req, 'index.html')
 
+
 # class CBView(View):
 #     def get(self, request):
 #         return HttpResponse("class based view")
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -17,3 +20,16 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['injectme'] = 'Basic injection'
         return context
+
+
+class SchoolListView(ListView):
+    # define you own name
+    context_object_name = 'schools'
+    model = models.School  # defined by default without context_object_name: school_list
+    # school_list
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School  # school by default (without context_object_name)
+    template_name = 'basic_app/school_detail.html'
