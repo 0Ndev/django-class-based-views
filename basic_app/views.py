445e5b062af297b2f8c3be12cd0168f6f2d73306
Natalia <natalialllepost@gmail.com>
2021-04-29 15:34:18 +0200

    CRUD

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+# from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import View, TemplateView, ListView, DetailView, UpdateView, DeleteView
 from . import models
 # from django.http import HttpResponse
 
@@ -33,3 +34,22 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School  # school by default (without context_object_name)
     template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(UpdateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+    template_name = 'basic_app/school_form.html'
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    # connect model
+    model = models.School
+    template_name = 'basic_app/school_conform_delete.html'
+    # go back to school list after delete
+    success_url = reverse_lazy("basic_app:list")
